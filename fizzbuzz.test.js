854a26dc5c09d1b9f5ab6e411b9b380fe5ed781f
Kevin <kevinpanjonjara@gmail.com>
2023-06-27 11:45:44 +0200

    fizzbuzz

---
+++ fizzbuzz.test.js
@@ -0,0 +1,22 @@
+const { createFizzBuzz } = require('./FizzBuzz');
+
+describe ("si el numero es multiplo de 3 y 5", () =>{
+
+test("saldra fizz", () =>{
+  expect(createFizzBuzz(9)).toBe('Fizz');
+
+});
+
+test("saldra buzz", () =>{
+  expect(createFizzBuzz(10)).toBe('Buzz');
+});
+
+test("saldra fizzbuzz", () => {
+  expect(createFizzBuzz(15)).toBe('FizzBuzz');
+});
+});
+
+test("debe ser un numero entero", () =>{
+  expect(createFizzBuzz(22.5)).toBe("No es un numero entero");
+})
+  
--- tests/FizzBuzz.test.js
+++
@@ -1,5 +0,0 @@
-describe('FizzBuzz', () => {
-    test('should return Fizz', () => {
-        
-    });
-});
